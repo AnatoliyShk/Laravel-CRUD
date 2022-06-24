27bdaaef386a9e5eaea7eeb7fb46bd15fc14b3a4
Anatoliy Shekera <tolikshekera@gmail.com>
2022-06-24 13:34:30 +0300

    edit validation

--- app/Http/Requests/Post/StoreRequest.php
+++ app/Http/Requests/Post/StoreRequest.php
@@ -16,7 +16,7 @@ class StoreRequest extends FormRequest
     {
         return [
             'title' => 'required|min:3|max:255',
-            'description' => 'required|min:3|max:255',
+            'description' => 'required|min:10',
             'image' => [
                 'required'
             ],

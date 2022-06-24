be4d21b92cff3ce0d360af79fc1acc3e3e490ed4
Anatoliy Shekera <tolikshekera@gmail.com>
2022-06-25 01:40:17 +0300

    update store image request

--- app/Http/Requests/Image/StoreImageRequest.php
+++ app/Http/Requests/Image/StoreImageRequest.php
@@ -13,7 +13,7 @@ class StoreImageRequest extends FormRequest
      */
     public function authorize()
     {
-        return $this->route('image')->post->user->id === auth()->id();
+        return true;
     }
 
     /**

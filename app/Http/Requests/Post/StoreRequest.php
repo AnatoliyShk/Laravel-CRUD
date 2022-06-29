10c3629f69766493fc700ab619003c7524477d45
Anatoliy Shekera <tolikshekera@gmail.com>
2022-06-29 13:20:55 +0300

    file size

--- app/Http/Requests/Post/StoreRequest.php
+++ app/Http/Requests/Post/StoreRequest.php
@@ -17,7 +17,7 @@ class StoreRequest extends FormRequest
         return [
             'title' => 'required|min:3|max:255',
             'description' => 'required|min:10',
-            'image' => 'required|file|max:1'
+            'image' => 'required|image|max:1'
         ];
     }
 
